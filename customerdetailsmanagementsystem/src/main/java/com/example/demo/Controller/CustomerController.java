dae89211967a1793e4420f2c72bd5919c0a33e51
IT21265242Zimmendra <99347087+IT21265242Zimmendra@users.noreply.github.com>
2022-12-09 22:36:30 +0530

    Update not successful

--- customerdetailsmanagementsystem/src/main/java/com/example/demo/Controller/CustomerController.java
+++ customerdetailsmanagementsystem/src/main/java/com/example/demo/Controller/CustomerController.java
@@ -8,10 +8,7 @@ import com.example.demo.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/customer")
@@ -49,6 +46,38 @@ public ResponseEntity saveCustomer(@RequestBody CustomerDTO customerDTO){
         return new ResponseEntity(responseDTO,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
+@PutMapping(value = "/UpdateCustomer")
+    public ResponseEntity updateCustomer(@RequestBody CustomerDTO customerDTO){
+        try{
+            String res = customerSevice.updateCustomer(customerDTO);
+            if(res.equals("00")){
+                responseDTO.setCode(VarList.Success);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(customerDTO);
+                return new ResponseEntity(responseDTO,HttpStatus.ACCEPTED);
+            } else if (res.equals("01")) {
+                responseDTO.setCode(VarList.No_data_found);
+                responseDTO.setMessage("Data not found");
+                responseDTO.setContent(customerDTO);
+                return new ResponseEntity(responseDTO,HttpStatus.BAD_REQUEST);
+            } else {
+                responseDTO.setCode(VarList.Fail);
+                responseDTO.setMessage("Error");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO,HttpStatus.BAD_REQUEST);
+
+            }
+        }catch (Exception ex){
+            responseDTO.setCode(VarList.Token_Error);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
+        
+}
+    
+    
 
 
 }
